a083dfb7a3cf2f31ffa5ea002b442cc6ec5d7bc0
Trillzdboy <chukwudubem7@gmail.com>
2022-05-29 12:17:14 +0100

    Added logo

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,20 @@
 import { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/meals.jpg';
+// import mealsImage from '../../assets/meals.jpg';
+import perfumeImage from '../../assets/banner.jpeg';
+import logo from '../../assets/LogoTrans.png';
 import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Berry's Fragrances</h1>
+        <img className={classes.logo} src={logo} alt="" />
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
-        <img src={mealsImage} alt='A slide of very beautiful perfumes!' />
+        <img src={perfumeImage} alt='A slide of very beautiful perfumes!' />
       </div>
     </Fragment>
   );
